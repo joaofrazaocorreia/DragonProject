d2e94b13d5f7193f4751635185f3e900cc173476
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2022-11-04 11:18:57 +0000

    added a couple more die for later use. Also renamed some variables for easier identification later

--- ICproject.py
+++ ICproject.py
@@ -1,15 +1,17 @@
 import random
 
-dice=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+d20=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+d8=[1,2,3,4,5,6,7,8]
+d4=[1,2,3,4]
+
 party=["Warrior","Priest","Rogue"]
 fainted=[]
 enemies=["Orc Warrior","Your mother"]
 
-
 def init(x):
     return x[4]
 
-d20= random.sample(dice,1)
+dice_1d20= random.sample(d20,1)
 
 #ALLY STATS
 warrior=[32,5,2,5,2] #HP, MP, AP, WP, INIT
@@ -20,6 +22,6 @@ rogue=[27,10,1,4,3]
 orcw=[15,0,2,2,2] #HP, MP, AP, WP, INIT
 yomama=[25,0,4,7,1]
 
-turnOrder= d20[0]+ int(init(warrior))
+turnOrder= dice_1d20[0]+ int(init(warrior))
 
 print(str(turnOrder))
