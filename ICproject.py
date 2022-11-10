f806494a89eaea359c2923dbddce92d94a2c7261
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2022-11-10 15:50:51 +0000

    Added the game loop's base: Defeat check and Victory check.

--- ICproject.py
+++ ICproject.py
@@ -7,9 +7,9 @@ d20=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 d8=[1,2,3,4,5,6,7,8]
 d4=[1,2,3,4]
 
-party=["Warrior","Priest","Rogue"]
+allies=["Warrior","Priest","Rogue"]
 fainted=[]
-enemies=["Orc Warrior","Orc Archer"]
+wave1=["Orc Warrior A","Orc Warrior B", "Orc Archer"]
 
 #-----------------------------------------------------------------------------------------------
 #DICE ROLLS - Commands for rolling die.
@@ -37,11 +37,12 @@ rogue=[27,10,1,4,4]
 
 #ENEMY STATS - The stats of every enemy encounterable.
 
-orcw=[15,0,2,2,2] #HP, MP, AP, WP, INIT
+orcw1=[15,0,2,2,2] #HP, MP, AP, WP, INIT
+orcw2=orcw1
 orca=[5,0,2,3,4]
 
 #-----------------------------------------------------------------------------------------------
-#BATTLE VALUES - These values determine the flow of the battles, such as turn order and stuff!
+#BATTLE VALUES - These values determine the flow of the battles, such as turn order and stuff
 
 def init(x):        #INITIATION - Calls a fighter's init value.
     return int(x[4])
@@ -51,35 +52,104 @@ def turnOrder(x):             #TURN ORDER - Defines who goes first each turn dur
 #-----------------------------------------------------------------------------------------------
 #BATTLE PHASES - Functions for the battle phases
 
-def initphase(allies,enemies):
+def initphase(allies,wave):
+
+    global order
+    order=[]
+    order.clear()
+
     for i in allies:
-        if i == party[0]:
+        if i == allies[0]:
             x=warrior
             global warriorInit
             warriorInit=turnOrder(x)
             print(str(i)+" rolled "+str(warriorInit))
-        elif i == party[2]:                                 #THESE ARE PLACEHOLDERS!!!!!!!!!!!!!
+
+        elif i == allies[2]:                                 #THESE ARE PLACEHOLDERS!!!!!!!!!!!!!
             x=rogue
             global rogueInit
-            rogueInit=turnOrder(x)                          #We'll replace the party and the enemies strings with the actual battle enemies once they're added
+            rogueInit=turnOrder(x)                          #We'll replace the allies and the enemies strings with the actual battle waves once they're added
             print(str(i)+" rolled "+str(rogueInit))
-        elif i == party[1]:
+
+        elif i == allies[1]:
             x=priest
             global priestInit
             priestInit=turnOrder(x)
             print(str(i)+" rolled "+str(priestInit))
 
-    for i in enemies:
-        if i == enemies[0]:
-            x=orcw
-            global orcwarriorInit
-            orcwarriorInit=turnOrder(x)
-            print(str(i)+" rolled "+str(orcwarriorInit))
-        elif i == enemies[1]:
+
+    for i in wave:
+        if i == wave[0]:
+            x=orcw1
+            global orcwarrior1Init
+            orcwarrior1Init=turnOrder(x)
+            print(str(i)+" rolled "+str(orcwarrior1Init))
+
+        if i == wave[1]:
+            x=orcw2
+            global orcwarrior2Init
+            orcwarrior2Init=turnOrder(x)
+            print(str(i)+" rolled "+str(orcwarrior2Init))
+
+        elif i == wave[2]:
             x=orca
             global orcarcherInit
             orcarcherInit=turnOrder(x)
             print(str(i)+" rolled "+str(orcarcherInit))
+    
+    order.append(warriorInit)
+    order.append(priestInit)
+    order.append(rogueInit)
 
-initphase(party,enemies)
-print(str(warriorInit))
+    order.append(orcwarrior1Init)
+    order.append(orcwarrior2Init)     #Adds all init values to the order list
+    order.append(orcarcherInit)
+
+    order.sort()            #Sorts the list from smallest to biggest values
+
+
+
+
+initphase(allies,wave1)
+print(str(order))
+
+#-----------------------------------------------------------------------------------------------
+#GAME LOOP - Keeps the game going
+
+game=True
+defeat=False #Variables to check the game state
+victory=False
+
+while game:
+
+    if defeat:
+        game=False
+        print("You have no more allies than can fight.")
+        print("You lost the battle!")
+        break
+    
+    elif victory:
+        game=False
+        print("All enemies have been defeated.")
+        print("You won the battle!")
+        break
+
+    else:
+        #initphase(party,wave1)
+
+
+
+        if not defeat:                            #DEFEAT CHECK - If the "allies" list is empty, it will not reset the defeat variable, and the game ends.
+            defeat=True
+            for i in allies:
+                defeat=False
+                print("There's still allies.")
+        
+        if not victory:                           #VICTORY CHECK - If the "wave1" list is empty, it will not reset the victory variable, and the game ends.
+            victory=True
+            for i in wave1:
+                victory=False
+                print("There's still enemies.")   #Defeat checks before victory, so you can't win with an empty party.
+
+
+
