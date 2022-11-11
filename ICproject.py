a2b3291a17e156aa2b4c2c21774eeec73b493946
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2022-11-11 11:38:51 +0000

    Fixed dice roll calls, and added even more comments.

--- ICproject.py
+++ ICproject.py
@@ -3,16 +3,16 @@ import random
 #-----------------------------------------------------------------------------------------------
 #VARIABLES AND LISTS - This part stores the die and all the characters available in the game.
 
-d20=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]     #Die values.
-d8=[1,2,3,4,5,6,7,8]
-d6=[1,2,3,4,5,6]
-d4=[1,2,3,4]
+d20=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]  #20 sided dice
+d8=[1,2,3,4,5,6,7,8]  #8 sided dice
+d6=[1,2,3,4,5,6]  #6 sided dice
+d4=[1,2,3,4]  #4 sided dice
 
-allies=["Warrior","Priest","Rogue"]                           #ALLIES - Allies in the party (More can be added later)
+allies=["Warrior","Priest","Rogue"]  #ALLIES - Allies in the party (More can be added later)
 
-fainted=[]                                                    #FAINTED - group for storing and registering characters who have been killed.
+fainted=[]  #FAINTED - group for storing and registering characters who have been killed.
 
-wave1=["Orc Warrior A","Orc Warrior B", "Orc Archer"]         #WAVE1 - Enemies in the first wave of monsters (More can be added later)
+wave1=["Orc Warrior A","Orc Warrior B", "Orc Archer"]  #WAVE1 - Enemies in the first wave of monsters (More can be added later)
 
 #-----------------------------------------------------------------------------------------------
 #DICE ROLLS - Commands for rolling die.
@@ -42,30 +42,32 @@ priest= [20,25,0,2,6]                         # ID=2
 rogue = [27,10,1,4,4]                         # ID=3
 
 
+#-----------------------------------------------------------------------------------------------
 #ENEMY STATS - The stats of every enemy encounterable.
 
 orcw1=[15,0,2,2,2] #HP, MP, AP, WP, INIT      ID=4
 orcw2=[15,0,2,2,2]                          # ID=5
 orca =[5, 0,1,4,4]                          # ID=6
 
+
 #-----------------------------------------------------------------------------------------------
 #BATTLE VALUES - These values determine the flow of the battles, such as turn order and stuff
 
-def init(x):                  #INIT - Calls a fighter's initiation value.
+def init(x):   #INIT - Calls a fighter's initiation value.
     return int(x[4])
 
-def turnOrder(x):             #TURN ORDER - Defines who goes first each turn during battle. Used to calculate Init rolls.
+def turnOrder(x):   #TURN ORDER - Defines who goes first each turn during battle. Used to calculate Init rolls.
     return rolld20(1) + init(x)
 
-def chooseEnemy():
-    global target             #CHOOSE ENEMY - Allows the player to choose an enemy target. Changes the "target" variable to an enemy of choice.
+def chooseEnemy():  #CHOOSE ENEMY - Allows the player to choose an enemy target. Changes the "target" variable to an enemy of choice.
+    global target
     print("")
     print("Target an enemy!")
     print(str(wave1))
     target=input()
     target=target.lower()
 
-def chooseAlly():             #CHOOSE ALLY - Allows the player to choose an allied target. Changes the "target" variable to an ally of choice.
+def chooseAlly():  #CHOOSE ALLY - Allows the player to choose an allied target. Changes the "target" variable to an ally of choice.
     global target
     print("")
     print("Target an ally!")
@@ -73,7 +75,7 @@ def chooseAlly():             #CHOOSE ALLY - Allows the player to choose an alli
     target=input()
     target=target.lower()
 
-def chooseSpell(charID):      #CHOOSE SPELL - Allows the player to choose a spell. Displays different options depending on the character ID given.
+def chooseSpell(charID):  #CHOOSE SPELL - Allows the player to choose a spell. Displays different options depending on the character ID given.
     global spell
     print("Choose a spell.")
     if charID == 1:
@@ -87,66 +89,66 @@ def chooseSpell(charID):      #CHOOSE SPELL - Allows the player to choose a spel
     spell=spell.lower()
 
 
-def calculateDamage(WP,AP):     #CALCULATE DAMAGE - Subtracts the given AP from the given WP. If the result is negative, it will return 0 instead.
+def calculateDamage(WP,AP):  #CALCULATE DAMAGE - Subtracts the given AP from the given WP. If the result is negative, it will return 0 instead.
     damage= WP-AP
-    if damage<0:                # (Used for ATTACK combat.)
+    if damage<0:             #(Used for ATTACK combat.)
         damage=0
     return damage
 
-def calculateValues(spell, WP):                #CALCULATE VALUES - Depending on the spell given (and on the given WP), calculates the spell's damage / healing.
+def calculateValues(spell, WP):  #CALCULATE VALUES - Depending on the spell given (and on the given WP), calculates the spell's damage / healing.
     if spell=="rushdown":
-        spellEffectValue= -1 * (WP+rolld4(1))  # (Used for MAGIC combat.)
+        spellEffectValue= -1 * (WP+rolld4()) #(Used for MAGIC combat.)
 
     elif spell=="mend":
-        spellEffectValue= WP + rolld6(1)
+        spellEffectValue= WP + rolld6()  #Positive values heal, Negative values deal damage (damage calculation uses a sum).
 
     elif spell=="exorcism":
-        spellEffectValue= -2 * rolld4(1)
+        spellEffectValue= -2 * rolld4()
 
     return spellEffectValue
 
 
 #-----------------------------------------------------------------------------------------------
-#BATTLE PHASES - Functions for the battle phases
+#INITIATION PHASE - Command for the init phase
 
 def initphase(allies,wave):
 
-    global order                                #Command for the init phase
+    global order
     order=[]
-    order.clear()
+    order.clear()  #Preemptively clears the list before rerolling, just in case of an error.
 
-    if "Warrior" in allies:
+    if "Warrior" in allies:  #Warrior rolls for init if he's alive.
         x=warrior
         global warriorInit
         warriorInit=turnOrder(x)
         print("Warrior rolled "+str(warriorInit))
 
-    if "Rogue" in allies:                                 
+    if "Rogue" in allies:  #Rogue rolls for init if he's alive.                     
         x=rogue
         global rogueInit
         rogueInit=turnOrder(x)                          
         print("Rogue rolled "+str(rogueInit))
 
-    if "Priest" in allies:
+    if "Priest" in allies:  #Priest rolls for init if he's alive.
         x=priest
         global priestInit
         priestInit=turnOrder(x)
         print("Priest rolled "+str(priestInit))
 
 
-    if "Orc Warrior A" in wave:
+    if "Orc Warrior A" in wave:  #Orc Warrior A rolls for init if he's alive.
         x=orcw1
         global orcwarriorAInit
         orcwarriorAInit=turnOrder(x)
         print("Orc Warrior A rolled "+str(orcwarriorAInit))
             
-    if "Orc Warrior B" in wave:
+    if "Orc Warrior B" in wave:  #Orc Warrior B rolls for init if he's alive.
         x=orcw2
         global orcwarriorBInit
         orcwarriorBInit=turnOrder(x)
         print("Orc Warrior B rolled "+str(orcwarriorBInit)) 
 
-    if "Orc Archer" in wave:
+    if "Orc Archer" in wave:  #Orc Archer rolls for init if he's alive.
         x=orca
         global orcarcherInit
         orcarcherInit=turnOrder(x)
@@ -162,13 +164,13 @@ def initphase(allies,wave):
 
     while orcwarriorBInit in order:
         orcwarriorBInit+=0.1
-    order.append(orcwarriorBInit)     #Adds all init values to the order list, adds 0.1 if the value already exists
+    order.append(orcwarriorBInit)  #Adds all init values to the "order" list, adds 0.1 if the value already exists.
 
-    while orcarcherInit in order:
+    while orcarcherInit in order:  #The fighters at the top have less priority than the fighters below them (i.e. Orc Archer has less priority than Rogue)
         orcarcherInit+=0.1
     order.append(orcarcherInit)
 
-    while rogueInit in order:        #IMPORTANT NOTE - DON'T CHANGE THIS ORDER!!!! THEY'RE BASED ON INIT VALUES, FROM HIGHEST TO LOWEST
+    while rogueInit in order:      #IMPORTANT NOTE - DON'T CHANGE THIS ORDER!!!! THEY'RE BASED ON INIT VALUES, FROM LOWEST TO HIGHEST
         rogueInit+=0.1
     order.append(rogueInit)
 
@@ -176,43 +178,47 @@ def initphase(allies,wave):
         priestInit+=0.1
     order.append(priestInit)
 
-    order.sort()            #Sorts the list from smallest to biggest values
-    #print(str(order))
+    order.sort()  #Sorts the list from smallest to biggest values
+    print(str(order))
+    print("")
 
-def attackphase(characterID):        #Command for the attack phase
+
+#-----------------------------------------------------------------------------------------------
+#ATTACK PHASE - Command for the attack phase
+
+def attackphase(characterID):
 
     if characterID==1:
         WP=warrior[3]
-    elif characterID==2:               #Assigns WP according to the ID given
+    elif characterID==2:  #Assigns WP according to the character ID given.
         WP=priest[3]
     elif characterID==3:
         WP=rogue[3]
 
     print("")
     print("What will you do?")
-    print("ATTACK / MAGIC")
+    print("ATTACK / MAGIC")   #Prompts the player's choice for a command.
     command=input()
     command=command.lower()
     print("")
 
-
     turn=True
 
-    if command=="attack":
+    if command=="attack":  #loop for the ATTACK command.
 
         while turn:
 
-            chooseEnemy()                      #player inputs an enemy and their stats will be used for combat
+            chooseEnemy()  #The player chooses an enemy and their stats will be used for melee combat.
             print("")
 
             if target=="orc warrior a" and "Orc Warrior A" not in fainted:
                 AP=orcw1[2]
                 dmg=calculateDamage(WP,AP)
-                orcw1[0]-=dmg
+                orcw1[0]-=dmg              #Calculates and subtracts damage from the target's hp.
                 print("Orc Warrior A took "+str(dmg)+" damage!")
                 print("")
                 if orcw1[0]<=0:
-                    wave1.remove("Orc Warrior A")
+                    wave1.remove("Orc Warrior A")   #Faints if health drops below 0
                     fainted.append("Orc Warrior A")
                     print("Orc Warrior A fainted!")
                     print("")
@@ -221,11 +227,11 @@ def attackphase(characterID):        #Command for the attack phase
             elif target=="orc warrior b" and "Orc Warrior B" not in fainted:
                 AP=orcw2[2]
                 dmg=calculateDamage(WP,AP)
-                orcw2[0]-=dmg
+                orcw2[0]-=dmg              #Calculates and subtracts damage from the target's hp.
                 print("Orc Warrior B took "+str(dmg)+" damage!")
                 print("")
                 if orcw2[0]<=0:
-                    wave1.remove("Orc Warrior B")
+                    wave1.remove("Orc Warrior B")   #Faints if health drops below 0
                     fainted.append("Orc Warrior B")
                     print("Orc Warrior B fainted!")
                     print("")
@@ -234,31 +240,33 @@ def attackphase(characterID):        #Command for the attack phase
             elif target=="orc archer" and "Orc Archer" not in fainted:
                 AP=orca[2]
                 dmg=calculateDamage(WP,AP)
-                orca[0]-=dmg
+                orca[0]-=dmg              #Calculates and subtracts damage from the target's hp.
                 print("Orc Archer took "+str(dmg)+" damage!")
                 print("")
                 if orca[0]<=0:
-                    wave1.remove("Orc Archer")
+                    wave1.remove("Orc Archer")   #Faints if health drops below 0
                     fainted.append("Orc Archer")
                     print("Orc Archer fainted!")
                     print("")
                 turn=False
 
             else:
-                print("That's not an enemy.")           #doesn't turn "attack" into false so it loops back.            
+                print("That's not an enemy.") #Causes error if the target input is unknown, and loops back to the prompt.            
                 print("")
     
-    elif command=="magic":
+    elif command=="magic": #loop for the MAGIC command.
         
         while turn:
             chooseSpell(characterID)
             print("")
-            if spell == "rushdown" and characterID==1:
+            if spell == "rushdown" and characterID==1:   #--RUSHDOWN-- spell
                 effectValue=calculateValues("rushdown",WP)
                 chooseEnemy()
                 print("")
                 if target=="orc warrior a" and "Orc Warrior A" not in fainted:
+
                     orcw1[0]+=effectValue
+
                     print("Orc Warrior A took "+str(-effectValue)+" damage!")
                     print("")
                     if orcw1[0]<=0:
@@ -269,7 +277,9 @@ def attackphase(characterID):        #Command for the attack phase
                     turn=False
 
                 elif target=="orc warrior b" and "Orc Warrior B" not in fainted:
+
                     orcw2[0]+=effectValue
+
                     print("Orc Warrior B took "+str(-effectValue)+" damage!")
                     print("")
                     if orcw2[0]<=0:
@@ -280,7 +290,9 @@ def attackphase(characterID):        #Command for the attack phase
                     turn=False
 
                 elif target=="orc archer" and "Orc Archer" not in fainted:
+
                     orca[0]+=effectValue
+
                     print("Orc Archer took "+str(-effectValue)+" damage!")
                     print("")
                     if orca[0]<=0:
@@ -290,43 +302,43 @@ def attackphase(characterID):        #Command for the attack phase
                         print("")
                     turn=False
 
-            elif spell == "rushdown":
+            elif spell == "rushdown":   #Causes error if not used by Warrior.
                 print("This character can't use this spell!")
 
 
-            elif spell == "mend" and characterID==2:
+            elif spell == "mend" and characterID==2:   #--MEND-- spell
                 effectValue=calculateValues("mend",WP)
                 chooseAlly()
                 print("")
 
-            elif spell == "mend":
+            elif spell == "mend":   #Causes error if not used by Priest.
                 print("This character can't use this spell!")
 
 
-            elif spell == "exorcism" and characterID==2:
+            elif spell == "exorcism" and characterID==2:  #--EXORCISM-- spell
                 effectValue=calculateValues("exorcism",WP)
                 chooseEnemy()
                 print("")
 
-            elif spell == "exorcism":
+            elif spell == "exorcism":   #Causes error if not used by Priest.
                 print("This character can't use this spell!")
 
 
-            elif spell == "sharpen" and characterID==3:
+            elif spell == "sharpen" and characterID==3:  #--SHARPEN-- spell
                 rogue[3]+=2
                 WP+=2
                 print("Rogue sharpens his weapon. +2 Weapon Power")
                 print("")
                 turn=False
 
-            elif spell == "sharpen":
+            elif spell == "sharpen":   #Causes error if not used by Rogue.
                 print("This character can't use this spell!")
 
             else:
-                print("That's not a known spell.")
+                print("That's not a known spell.")   #Causes error if the spell input is unknown, and loops back to the prompt.
     
     else:
-        print("Unknown Command.")
+        print("Unknown Command.")  #Causes error if the battle command is unknown, and lopps back to the prompt.
         attackphase(characterID)
 
 
@@ -350,42 +362,43 @@ while game:
 
     initphase(allies,wave1)
 
-    while len(order)>0:
-        if warriorInit==order[-1] and "Warrior" not in fainted:
+    while len(order)>0:  #Loops until all characters have ended their attack phase.
+
+        if warriorInit==order[-1] and "Warrior" not in fainted:  #Starts the Warrior's turn if he's alive.
             print("It's Warrior's turn.")
             order.remove(order[-1])
             attackphase(1)
         
-        elif priestInit==order[-1] and "Priest" not in fainted:
+        elif priestInit==order[-1] and "Priest" not in fainted:  #Starts the Priests's turn if he's alive.
             print("It's Priest's turn.")
             order.remove(order[-1])
             attackphase(2)
         
-        elif rogueInit==order[-1] and "Rogue" not in fainted:
+        elif rogueInit==order[-1] and "Rogue" not in fainted:  #Starts the Rogue's turn if he's alive.
             print("It's Rogue's turn.")
             order.remove(order[-1])
             attackphase(3)
 
-        elif orcwarriorAInit==order[-1] and "Orc Warrior A" not in fainted:
+        elif orcwarriorAInit==order[-1] and "Orc Warrior A" not in fainted:  #Starts the Orc Warrior A's turn if he's alive.
             print("It's Orc Warrior A's turn.")
             order.remove(order[-1])
                 
-        elif orcwarriorBInit==order[-1] and "Orc Warrior B" not in fainted:
+        elif orcwarriorBInit==order[-1] and "Orc Warrior B" not in fainted:  #Starts the Orc Warrior B's turn if he's alive.
             print("It's Orc Warrior B's turn.")
             order.remove(order[-1])
 
-        elif orcarcherInit==order[-1] and "Orc Archer" not in fainted:
+        elif orcarcherInit==order[-1] and "Orc Archer" not in fainted:   #Starts the Orc Archer's turn if he's alive.
             print("It's Orc Archer's turn.")
             order.remove(order[-1])
 
-    if len(allies)==0:                            #DEFEAT CHECK - If the "allies" list is empty, the game ends.
+    if len(allies)==0: #DEFEAT CHECK - If the "allies" list is empty, the game ends and the player loses.
         game=False
         print("You have no more allies than can fight.")
         print("You lost the battle!")
         break
  
-    if len(wave1)==0:                              #VICTORY CHECK - If the "wave1" list is empty, the game ends.
+    if len(wave1)==0:  #VICTORY CHECK - If the "wave1" list is empty, the game ends and the player wins.
         game=False
         print("All enemies have been defeated.")
         print("You won the battle!")
-        break                           #Defeat checks before victory, so you can't win with an empty party.
+        break          #(Defeat checks before Victory, so the player can't win with an empty party.)
